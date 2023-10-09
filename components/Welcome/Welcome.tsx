fa3bf6222d42d234cda2ae267bd2d86f66e409cf
Pheon-Dev <devpheon@gmail.com>
2023-10-09 16:19:09 +0300

    [+] AppShell [+] Header

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -7,12 +7,17 @@ export function Welcome() {
       <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          Cargo App
         </Text>
       </Title>
       <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
+        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
+        ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
+        officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate
+        dolor minim nulla est proident. Nostrud officia pariatur ut officia. Culpa proident
+        adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua
+        reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint
+        cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
           this guide
         </Anchor>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,35 @@
 import '@mantine/core/styles.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, Text } from '@mantine/core';
 import { theme } from '../theme';
+import { AppShell, Burger, Group, Skeleton, rem } from '@mantine/core';
+import { useHeadroom } from '@mantine/hooks';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const pinned = useHeadroom({ fixedAt: 120 });
   return (
     <MantineProvider theme={theme}>
-      <Head>
-        <title>Mantine Template</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
-        <link rel="shortcut icon" href="/favicon.svg" />
-      </Head>
-      <Component {...pageProps} />
+      <AppShell
+        withBorder={true}
+        header={{ height: 60, collapsed: !pinned, offset: false }}
+        padding="md"
+      >
+        <AppShell.Header>Cargo App</AppShell.Header>
+        {/* <AppShell.Navbar>Navbar</AppShell.Navbar> */}
+        <AppShell.Main pt={`calc(${rem(60)} + var(--mantine-spacing-md))`}>
+          <Head>
+            <title>Cargo App</title>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+            />
+            <link rel="shortcut icon" href="/favicon.svg" />
+          </Head>
+
+          <Component {...pageProps} />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
